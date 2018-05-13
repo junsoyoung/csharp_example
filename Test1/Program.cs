5c51117dae87e2fe45af6aaf4b249b37d6f6343a
junsoyoung <jbackdoo@gmail.com>
2018-05-14 00:40:11 +0900

    C# 연습 프로젝트 생성

---
+++ Test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ABC");
+            List<int> list = new List<int>();
+            list.Add(0);
+            list.Add(10);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.ReadKey();
+
+        }
+    }
+}
